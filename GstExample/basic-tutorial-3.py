40b6fbee3f32eb63f2dfa531988d77065ffadc32
waelatrash <wa@lombego.de>
2018-06-20 02:10:55 +0200

    pad added handler

--- GstExample/basic-tutorial-3.py
+++ GstExample/basic-tutorial-3.py
@@ -19,8 +19,8 @@ from gi.repository import GObject, Gst
 GObject.threads_init()
 Gst.init(None)
 
-
 data = dict()
+
 
 # Handler for the pad-added signal
 
@@ -37,8 +37,8 @@ def pad_added_handler(src, new_pad, dataPAD):
     # Check the new pad's type
     new_pad_type = new_pad.query_caps(None).to_string()
 
-    if not new_pad_type.startswith("audio/x-raw"):
-        print("  It has type '%s' which is not raw audio. Ignoring." %
+    if not new_pad_type.startswith("video/x-raw"):
+        print("  It has type '%s' which is not raw video. Ignoring." %
               new_pad_type)
         return
 
@@ -47,62 +47,67 @@ def pad_added_handler(src, new_pad, dataPAD):
     return
 
 
-# Create the elements
-data["source"] = Gst.ElementFactory.make("uridecodebin", "source")
-data["convert"] = Gst.ElementFactory.make("audioconvert", "convert")
-data["sink"] = Gst.ElementFactory.make("autoaudiosink", "sink")
+def main():
+    # Create the elements
+    data["source"] = Gst.ElementFactory.make("uridecodebin", "source")
+    data["convert"] = Gst.ElementFactory.make("videoconvert", "convert")
+    data["sink"] = Gst.ElementFactory.make("autovideosink", "video-output")
 
-# Create the empty pipeline
-pipeline = Gst.Pipeline.new("test-pipeline")
+    # Create the empty pipeline
+    pipeline = Gst.Pipeline.new("test-pipeline")
 
-if not data["source"] or not data["convert"] or not data["sink"] or not pipeline:
-    print("Not all elements could be created.", file=sys.stderr)
-    exit(-1)
+    if not data["source"] or not data["convert"] or not data["sink"] or not pipeline:
+        print("Not all elements could be created.", file=sys.stderr)
+        exit(-1)
 
-# Build the pipeline
-# Note that we are NOT linking the source at this point. We will do it later.
-pipeline.add(data["source"])
-pipeline.add(data["convert"])
-pipeline.add(data["sink"])
-if not Gst.Element.link(data["convert"], data["sink"]):
-    print("Elements could not be linked.", file=sys.stderr)
-    exit(-1)
+    # Build the pipeline
+    # Note that we are NOT linking the source at this point. We will do it later.
+    pipeline.add(data["source"])
+    pipeline.add(data["convert"])
+    pipeline.add(data["sink"])
+    if not Gst.Element.link(data["convert"], data["sink"]):
+        print("Elements could not be linked.", file=sys.stderr)
+        exit(-1)
 
-# Set the URI to play
-data["source"].set_property(
-    "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm")
+    # Set the URI to play
+    data["source"].set_property(
+        "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm")
 
-# Connect to the pad-added signal
-data["source"].connect("pad-added", pad_added_handler, data)
+    # Connect to the pad-added signal
+    data["source"].connect("pad-added", pad_added_handler, data)
 
-# Start playing
-ret = pipeline.set_state(Gst.State.PLAYING)
-if ret == Gst.StateChangeReturn.FAILURE:
-    print("Unable to set the pipeline to the playing state.", file=sys.stderr)
-    exit(-1)
+    # Start playing
+    ret = pipeline.set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        print("Unable to set the pipeline to the playing state.", file=sys.stderr)
+        exit(-1)
 
-# Wait until error or EOS
-bus = pipeline.get_bus()
+    # Wait until error or EOS
+    bus = pipeline.get_bus()
 
-# Parse message
-while True:
-    message = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS)
-    if message.type == Gst.MessageType.ERROR:
-        err, debug = message.parse_error()
-        print("Error received from element %s: %s" % (
-            message.src.get_name(), err), file=sys.stderr)
-        print("Debugging information: %s" % debug, file=sys.stderr)
-        break
-    elif message.type == Gst.MessageType.EOS:
-        print("End-Of-Stream reached.")
-        break
-    elif message.type == Gst.MessageType.STATE_CHANGED:
-        if isinstance(message.src, Gst.Pipeline):
-            old_state, new_state, pending_state = message.parse_state_changed()
-            print("Pipeline state changed from %s to %s." %
-                  (old_state.value_nick, new_state.value_nick))
-    else:
-        print("Unexpected message received.", file=sys.stderr)
+    # Parse message
+    while True:
+        message = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE,
+                                         Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR | Gst.MessageType.EOS)
+        if message.type == Gst.MessageType.ERROR:
+            err, debug = message.parse_error()
+            print("Error received from element %s: %s" % (
+                message.src.get_name(), err), file=sys.stderr)
+            print("Debugging information: %s" % debug, file=sys.stderr)
+            break
+        elif message.type == Gst.MessageType.EOS:
+            print("End-Of-Stream reached.")
+            break
+        elif message.type == Gst.MessageType.STATE_CHANGED:
+            if isinstance(message.src, Gst.Pipeline):
+                old_state, new_state, pending_state = message.parse_state_changed()
+                print("Pipeline state changed from %s to %s." %
+                      (old_state.value_nick, new_state.value_nick))
+        else:
+            print("Unexpected message received.", file=sys.stderr)
 
-# Free resources
-pipeline.set_state(Gst.State.NULL)
+    # Free resources
+    pipeline.set_state(Gst.State.NULL)
+
+
+main()
--- GstExample/basic-tutorial-4.py
+++ GstExample/basic-tutorial-4.py
@@ -11,11 +11,21 @@ http://docs.gstreamer.com/display/GstSDK/Basic+tutorial+4%3A+Time+management
 
 import sys
 import gi
+
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
+
 Gst.init(None)
 
 # Python version of GST_TIME_ARGS
+
+data = dict()
+
+data["playing"] = False
+data["terminate"] = False
+data["seek_enabled"] = False
+data["seek_done"] = False
+data["duration"] = Gst.CLOCK_TIME_NONE
 
 
 def convert_ns(t):
@@ -30,20 +40,20 @@ def convert_ns(t):
 
 
 def handle_message(data, msg):
-    if message.type == Gst.MessageType.ERROR:
-        err, debug = message.parse_error()
-        print("Error received from element %s: %s" % (message.src.get_name(), err), file=sys.stderr)
+    if msg.type == Gst.MessageType.ERROR:
+        err, debug = msg.parse_error()
+        print("Error received from element %s: %s" % (msg.src.get_name(), err), file=sys.stderr)
         print("Debugging information: %s" % debug, file=sys.stderr)
         data["terminate"] = True
-    elif message.type == Gst.MessageType.EOS:
+    elif msg.type == Gst.MessageType.EOS:
         print("End-Of-Stream reached.")
         data["terminate"] = True
-    elif message.type == Gst.MessageType.DURATION_CHANGED:
+    elif msg.type == Gst.MessageType.DURATION_CHANGED:
         # The duration has changed, mark the current one as invalid
         data["duration"] = Gst.CLOCK_TIME_NONE
-    elif message.type == Gst.MessageType.STATE_CHANGED:
-        if message.src == data["playbin"]:
-            old_state, new_state, pending_state = message.parse_state_changed()
+    elif msg.type == Gst.MessageType.STATE_CHANGED:
+        if msg.src == data["playbin"]:
+            old_state, new_state, pending_state = msg.parse_state_changed()
             print("Pipeline state changed from %s to %s." % (old_state.value_nick, new_state.value_nick))
             data["playing"] = (new_state == Gst.State.PLAYING)
             if data["playing"]:
@@ -58,71 +68,67 @@ def handle_message(data, msg):
                     print("Seeking query failed.", file=sys.stderr)
     else:
         print("Unexpected message received.", file=sys.stderr)
- 
-
-data = dict()
-
-data["playing"] = False
-data["terminate"] = False
-data["seek_enabled"] = False
-data["seek_done"] = False
-data["duration"] = Gst.CLOCK_TIME_NONE
-
-# Create the elements
-data["playbin"] = Gst.ElementFactory.make("playbin", "playbin")
-
-if not data["playbin"]:
-    print("Not all elements could be created.", file=sys.stderr)
-    exit(-1)
-
-# Set the URI to play
-data["playbin"].set_property(
-    "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm")
-
-# Start playing
-ret = data["playbin"].set_state(Gst.State.PLAYING)
-if ret == Gst.StateChangeReturn.FAILURE:
-    print("Unable to set the pipeline to the playing state.", file=sys.stderr)
-    exit(-1)
-
-# Listen to the bus
-bus = data["playbin"].get_bus()
-while not data["terminate"]:
-    message = bus.timed_pop_filtered(100 * Gst.MSECOND,
-                                     Gst.MessageType.STATE_CHANGED | 
-                                     Gst.MessageType.ERROR | 
-                                     Gst.MessageType.EOS | 
-                                     Gst.MessageType.DURATION_CHANGED)
-
-    # Parse message
-    if message:
-        handle_message(data, message)
-    else:
-        if data["playing"]:
-            fmt = Gst.Format.TIME
-            current = -1
-            # Query the current position of the stream
-            _, current = data['playbin'].query_position(fmt)
-            if not current:
-                print("Could not query current position", file=sys.stderr)
-
-            # If we didn't know it yet, query the stream duration
-            if data["duration"] == Gst.CLOCK_TIME_NONE:
-                _, data["duration"] = data['playbin'].query_duration(fmt)
-                if not data["duration"]:
-                    print("Could not query current duration", file=sys.stderr)
-
-            print("Position %s / %s\r" % (
-                convert_ns(current), convert_ns(data["duration"])), end=' ')
-            sys.stdout.flush()
-
-            # If seeking is enabled, we have not done it yet, and the time is
-            # right, seek
-            if data["seek_enabled"] and not data["seek_done"] and current > 10 * Gst.SECOND:
-                print("\nReached 10s, performing seek...")
-                data['playbin'].seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT, 30 * Gst.SECOND)
-                data["seek_done"] = True
 
 
-# Free resources
-data["playbin"].set_state(Gst.State.NULL)
+def main():
+    # Create the elements
+    data["playbin"] = Gst.ElementFactory.make("playbin", "playbin")
+
+    if not data["playbin"]:
+        print("Not all elements could be created.", file=sys.stderr)
+        exit(-1)
+
+    # Set the URI to play
+    data["playbin"].set_property(
+        "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm")
+
+    # Start playing
+    ret = data["playbin"].set_state(Gst.State.PLAYING)
+    if ret == Gst.StateChangeReturn.FAILURE:
+        print("Unable to set the pipeline to the playing state.", file=sys.stderr)
+        exit(-1)
+
+    # Listen to the bus
+    bus = data["playbin"].get_bus()
+    while not data["terminate"]:
+        message = bus.timed_pop_filtered(100 * Gst.MSECOND,
+                                         Gst.MessageType.STATE_CHANGED |
+                                         Gst.MessageType.ERROR |
+                                         Gst.MessageType.EOS |
+                                         Gst.MessageType.DURATION_CHANGED)
+
+        # Parse message
+        if message:
+            handle_message(data, message)
+        else:
+            if data["playing"]:
+                fmt = Gst.Format.TIME
+                current = -1
+                # Query the current position of the stream
+                _, current = data['playbin'].query_position(fmt)
+                if not current:
+                    print("Could not query current position", file=sys.stderr)
+
+                # If we didn't know it yet, query the stream duration
+                if data["duration"] == Gst.CLOCK_TIME_NONE:
+                    _, data["duration"] = data['playbin'].query_duration(fmt)
+                    if not data["duration"]:
+                        print("Could not query current duration", file=sys.stderr)
+
+                print("Position %s / %s\r" % (
+                    convert_ns(current), convert_ns(data["duration"])), end=' ')
+                sys.stdout.flush()
+
+                # If seeking is enabled, we have not done it yet, and the time is
+                # right, seek
+                if data["seek_enabled"] and not data["seek_done"] and current > 10 * Gst.SECOND:
+                    print("\nReached 10s, performing seek...")
+                    data['playbin'].seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,
+                                                30 * Gst.SECOND)
+                    data["seek_done"] = True
+
+    # Free resources
+    data["playbin"].set_state(Gst.State.NULL)
+
+
+main()
