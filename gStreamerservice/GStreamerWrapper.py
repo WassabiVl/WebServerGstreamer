508045db467eaedfe7c362b336534b5fef08e327
SangMinhNguyen <sang.nguyen.minh@uni-weimar.de>
2018-07-11 13:14:55 +0200

    no message

--- gStreamerservice/GStreamerWrapper.py
+++ gStreamerservice/GStreamerWrapper.py
@@ -23,7 +23,9 @@ class GStreamerWrapper:
         if self.pipeline_string != "":
             self.pipeline_string += " "
 
-        self.pipeline_string += "udpsrc port=" + str(port) + " caps=\"application/x-rtp, width=640, height=480, framerate=60/1\" buffer-size=100000 ! rtpjitterbuffer ! rtpgstdepay ! jpegdec ! alpha method=green ! mixer.sink_" + str(port)
+        self.pipeline_string += "udpsrc port=" + str(port) + \
+                                " caps=\"application/x-rtp, width=640, height=480, framerate=60/1\" buffer-size=100000 ! rtpjitterbuffer ! rtpgstdepay ! jpegdec ! alpha method=green ! mixer.sink_" + \
+                                str(port)
 
     def add_dest(self, sink_ip):
         # for testing
