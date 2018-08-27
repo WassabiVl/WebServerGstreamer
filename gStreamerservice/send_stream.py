5c46ccb3598acff3fd7cd968a71f0cdeeb319b41
SangMinhNguyen <sang.nguyen.minh@uni-weimar.de>
2018-08-27 11:17:48 +0200

    script

--- gStreamerservice/send_stream.py
+++ gStreamerservice/send_stream.py
@@ -8,18 +8,8 @@ gi.require_version('Gst', '1.0')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gst, Gtk
 
-
-# Build the pipeline
-try:
-    print(str(sys.argv))
-    url = sys.argv[1] + ":8888/get_port"
-    print(url)
-    r = requests.get("http://" + url)
-    print(r.text)
-    Gst.init(None)
-    pipeline = Gst.parse_launch('videotestsrc pattern=' + sys.argv[2] +
-                                ' background-color=0x00ff00 foreground-color=0x0000ff ! x264enc bitrate=200 speed-preset=superfast tune=zerolatency ! queue ! rtph264pay config-interval=1 ! queue ! udpsink host="' +
-                                sys.argv[1] + '" port=' + r.text)
+def run_pipeline(script):
+    pipeline = Gst.parse_launch(script)
 
     # Start playing
     pipeline.set_state(Gst.State.PAUSED)
@@ -43,6 +33,19 @@ try:
 
     # Free resources
     pipeline.set_state(Gst.State.NULL)
+
+# Build the pipeline
+try:
+    print(str(sys.argv))
+    url = sys.argv[1] + ":8888/get_port"
+    print(url)
+    r = requests.get("http://" + url)
+    print(r.text)
+    Gst.init(None)
+    run_pipeline('videotestsrc pattern=' + sys.argv[2] +
+                    ' background-color=0x00ff00 foreground-color=0x0000ff ! x264enc bitrate=200 speed-preset=superfast tune=zerolatency ! queue ! rtph264pay config-interval=1 ! queue ! udpsink host="' +
+                    sys.argv[1] + '" port=' + r.text)
+    run_pipeline('udpsrc port=6000 caps="application/x-rtp, payload=96,clock-rate=90000, framerate=60/1, width=500, height=300" ! rtpjitterbuffer drop-on-latency=false latency=500 ! rtpmp4vdepay ! avdec_mpeg4 ! queue ! videoconvert ! queue ! xvimagesink')
 
 except Exception as e:
     print(e)
