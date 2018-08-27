acd367a294c5338e5404750e93a432eddc5d965e
SangMinhNguyen <sang.nguyen.minh@uni-weimar.de>
2018-08-27 11:24:43 +0200

    removed bus check

--- gStreamerservice/send_stream.py
+++ gStreamerservice/send_stream.py
@@ -16,24 +16,6 @@ def run_pipeline(script):
     print('playing')
     pipeline.set_state(Gst.State.PLAYING)
 
-    # Wait until error or EOS
-    bus = pipeline.get_bus()
-    msg = bus.timed_pop_filtered(
-        Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
-    if msg:
-        if msg.type == Gst.MessageType.ERROR:
-            err, debug = msg.parse_error()
-            print("Error received from element %s: %s" % (
-                msg.src.get_name(), err), file=sys.stderr)
-            print("Debugging information: %s" % debug, file=sys.stderr)
-        elif msg.type == Gst.MessageType.EOS:
-            print("End-Of-Stream reached.")
-        else:
-            print("Unexpected message received.", file=sys.stderr)
-
-    # Free resources
-    pipeline.set_state(Gst.State.NULL)
-
 # Build the pipeline
 try:
     print(str(sys.argv))
@@ -45,7 +27,7 @@ try:
     run_pipeline('videotestsrc pattern=' + sys.argv[2] +
                     ' background-color=0x00ff00 foreground-color=0x0000ff ! x264enc bitrate=200 speed-preset=superfast tune=zerolatency ! queue ! rtph264pay config-interval=1 ! queue ! udpsink host="' +
                     sys.argv[1] + '" port=' + r.text)
-    run_pipeline('udpsrc port=6000 caps="application/x-rtp, payload=96,clock-rate=90000, framerate=60/1" ! rtpjitterbuffer drop-on-latency=false latency=500 ! rtpmp4vdepay ! avdec_mpeg4 ! queue ! videoconvert ! queue ! xvimagesink')
+    run_pipeline('udpsrc port=6000 caps="application/x-rtp, payload=96,clock-rate=90000, framerate=60/1" ! rtpjitterbuffer drop-on-latency=false latency=500 ! rtpmp4vdepay ! avdec_mpeg4 ! queue ! videoconvert ! queue ! xvimagesink sync=false')
 
 except Exception as e:
     print(e)
